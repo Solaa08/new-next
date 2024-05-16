f9c4fdb6653cc8633fde808650f29023d0601e25
Loick T <58403379+Solaa08@users.noreply.github.com>
2024-05-16 18:07:47 +0200

    feat: button & input nectron

---
+++ src/app/nectron/Button.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx';
+import React, { PropsWithChildren, ReactElement } from 'react';
+
+type ButtonWithChildren = PropsWithChildren<
+React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+>;
+
+interface ButtonProps extends ButtonWithChildren {
+  className?: string;
+}
+
+export default function Button({
+  children,
+  className,
+  ...props
+}: ButtonProps): ReactElement {
+  return (
+    <button
+      className={clsx(className, 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded')}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
