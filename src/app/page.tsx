60385f674785d847066d188ade35a8b50c4ddcad
Loick T <58403379+Solaa08@users.noreply.github.com>
2024-05-21 18:10:06 +0200

    Input (#5)
    * start input
    
    * fix: input style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 
 import Button from "./nectron/Button";
+import Input from "./nectron/Input";
 import { VARIANT } from "./nectron/variant";
 
 export default function Home() {
   return (
     <main className="flex bg-white min-h-screen flex-col items-center justify-between p-24 text-sm">
-      <Button variant={VARIANT.PRIMARY}>Valider</Button>
-      <Button variant={VARIANT.SECONDARY}>Valider</Button>
-      <Button variant={VARIANT.DANGER}>Valider</Button>
-      <Button variant={VARIANT.WARNING}>Valider</Button>
-      <Button variant={VARIANT.SUCCESS}>Valider</Button>
+      <Input></Input>
     </main>
   );
 }
