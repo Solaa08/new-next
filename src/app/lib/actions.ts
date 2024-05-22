a9b7589f01600f1579669260d93a3edf19151257
Loick T <58403379+Solaa08@users.noreply.github.com>
2024-05-22 15:49:36 +0200

    feat(game): add loader

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,14 +1,20 @@
 'use server'
 import axios from "axios";
 
+export async function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 export async function createGame(formData: FormData) {
 
     const rawFormData = {
       name: formData.get('name'),
       state: formData.get('state'),
     };
-    axios.post("http://localhost/new-next/back/", rawFormData)
+    await sleep(1000)
+    await axios.post("http://localhost/new-next/back/", rawFormData)
       .then((response) => {
         console.log(response);
       });
   }
+
