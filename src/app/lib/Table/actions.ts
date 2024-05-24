7fe22b677d5fa2636e2fa15a10d6667ccb8090a1
Loick T <58403379+Solaa08@users.noreply.github.com>
2024-05-24 15:15:34 +0200

    fix(games): console.log

--- src/app/lib/Table/actions.ts
+++ src/app/lib/Table/actions.ts
@@ -3,6 +3,5 @@ import axios from "axios";
 
 export async function getGames() {
     const response = await axios.get("http://localhost/new-next/back/")
-    console.log(response.data) 
     return response.data
   }
